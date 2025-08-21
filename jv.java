d5f8c1ebd763dca9e9ae2439ee5b961815731bd9
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2025-08-21 14:27:13 +0530

    Add files via upload

---
+++ jv.java
@@ -0,0 +1,15 @@
+package j.java;
+
+
+
+
+    
+public class jv {
+    public static void main(String[] args) {
+        
+        System.out.println("This is a jv file.");
+        System.out.println("It is used to demonstrate a simple Java program.");
+        System.out.println("Java is a versatile programming language used for various applications.");
+    }
+  
+}
